fbaaf75d23ec46df5470fed8e84e38496f3cbefa
Erik Dubois <erik.dubois@gmail.com>
2016-12-03 16:55:26 +0100

    Comment :  on 16:55:26 03-12-2016

--- theme-arc-colora-collection-3.0-v1.sh
+++ theme-arc-colora-collection-3.0-v1.sh
@@ -57,14 +57,14 @@
 # if there is no hidden folder then make one
 [ -d $HOME"/.themes" ] || mkdir -p $HOME"/.themes"
 
-rm -rf /tmp/Mint-Y-Colora-Theme-Collection
-git clone https://github.com/erikdubois/Mint-Y-Colora-Theme-Collection /tmp/Mint-Y-Colora-Theme-Collection
-find /tmp/Mint-Y-Colora-Theme-Collection -maxdepth 1 -type f -exec rm -rf '{}' \;
-cp -r /tmp/Mint-Y-Colora-Theme-Collection/Cinnamon\ 3.0/* ~/.themes/
-rm -rf /tmp/Mint-Y-Colora-Theme-Collection
 
+rm -rf /tmp/Arc-Theme-Colora-Collection
+git clone https://github.com/erikdubois/Arc-Theme-Colora-Collection /tmp/Arc-Theme-Colora-Collection
+find /tmp/Arc-Theme-Colora-Collection -maxdepth 1 -type f -exec rm -rf '{}' \;
+cp -r /tmp/Arc-Theme-Colora-Collection/Cinnamon\ 3.0/* ~/.themes/
+rm -rf /tmp/Arc-Theme-Colora-Collection
 
 
 echo "################################################################"
-echo "###############      Mint-y-themes installed        ############"
+echo "################## arc colora themes installed   ###############"
 echo "################################################################"
